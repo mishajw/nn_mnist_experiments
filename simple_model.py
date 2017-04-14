69c86f9fdd6887cf9b6064e03c51434bcfdbb28b
Misha Wagner <mishajw@gmail.com>
2017-04-14 15:23:20 +0100

    Use named scope for layers

--- simple_model.py
+++ simple_model.py
@@ -17,14 +17,15 @@ class SimpleModel:
         current_input = self.image_input
 
         for i, hidden_layer_size in enumerate(layers):
-            weights = tf.Variable(
-                tf.zeros([int(current_input.shape[1]), hidden_layer_size]),
-                "weights" + str(i))
+            with tf.name_scope("layer" + str(i)):
+                weights = tf.Variable(
+                    tf.zeros([int(current_input.shape[1]), hidden_layer_size]),
+                    name="weights")
 
-            biases = tf.Variable(
-                tf.zeros([hidden_layer_size]),
-                "biases" + str(i))
+                biases = tf.Variable(
+                    tf.zeros([hidden_layer_size]),
+                    name="biases")
 
-            current_input = tf.nn.sigmoid(tf.matmul(current_input, weights) + biases)
+                current_input = tf.nn.sigmoid(tf.matmul(current_input, weights) + biases)
 
         self.guesses = tf.nn.softmax(current_input)
