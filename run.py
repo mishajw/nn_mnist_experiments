039db424ade47029bfe5f0be4ad7d4f7f549fcf2
Misha Wagner <mishajw@gmail.com>
2017-04-14 14:39:08 +0100

    Add initial simple model with run function, based of tensorflow example

---
+++ run.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from simple_model import SimpleModel
+from tensorflow.examples.tutorials.mnist import input_data
+import argparse
+import tensorflow as tf
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
+                    help='Directory for storing input data')
+
+
+def main():
+    args = parser.parse_args()
+
+    # Import data
+    mnist = input_data.read_data_sets(args.data_dir, one_hot=True)
+
+    # Get model
+    model = SimpleModel()
+
+    # Define loss and optimizer
+    truth = tf.placeholder(tf.float32, [None, 10])
+    cross_entropy = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=truth, logits=model.guesses))
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+    sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
+
+    # Train
+    for _ in range(10000):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={model.image_input: batch_xs, truth: batch_ys})
+
+    # Test trained model
+    correct_prediction = tf.equal(tf.argmax(model.guesses, 1), tf.argmax(truth, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print(sess.run(
+        accuracy,
+        feed_dict={
+            model.image_input: mnist.test.images,
+            truth: mnist.test.labels
+        }))
+
+
+if __name__ == '__main__':
+    main()
---
+++ simple_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+
+
+class SimpleModel:
+    HIDDEN_LAYER_SIZES = [50, 10]
+
+    def __init__(self):
+        self.image_input = tf.placeholder(tf.float32, [None, 784])
+
+        current_input = self.image_input
+
+        for i, hidden_layer_size in enumerate(self.HIDDEN_LAYER_SIZES):
+            weights = tf.Variable(tf.zeros([current_input.shape[1], hidden_layer_size]), "weights" + str(i))
+            biases = tf.Variable(tf.zeros([hidden_layer_size]), "biases" + str(i))
+            current_input = tf.nn.sigmoid(tf.matmul(current_input, weights) + biases)
+
+        self.guesses = tf.nn.softmax(current_input)
