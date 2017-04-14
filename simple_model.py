1c55eb0727163be68d2beaca603babc47c39c806
Misha Wagner <mishajw@gmail.com>
2017-04-14 15:00:24 +0100

    Use random_normal for initialising weights and biases

--- simple_model.py
+++ simple_model.py
@@ -12,8 +12,14 @@ class SimpleModel:
         current_input = self.image_input
 
         for i, hidden_layer_size in enumerate(self.HIDDEN_LAYER_SIZES):
-            weights = tf.Variable(tf.zeros([current_input.shape[1], hidden_layer_size]), "weights" + str(i))
-            biases = tf.Variable(tf.zeros([hidden_layer_size]), "biases" + str(i))
+            weights = tf.Variable(
+                tf.zeros([int(current_input.shape[1]), hidden_layer_size]),
+                "weights" + str(i))
+
+            biases = tf.Variable(
+                tf.zeros([hidden_layer_size]),
+                "biases" + str(i))
+
             current_input = tf.nn.sigmoid(tf.matmul(current_input, weights) + biases)
 
         self.guesses = tf.nn.softmax(current_input)
